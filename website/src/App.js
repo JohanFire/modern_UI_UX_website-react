d53e8db61d1153e14e300e4f110fe86706661f0e
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-01-01 17:31:36 -0600

    Import all components with 1 line of code

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// you can import components with this line, but in this case I will not do it like this
+// I will import the components inside our containers
+import { Article, Brand, CTA, Feature, Navbar } from './components'
+
 const App = () => {
     return (
         <div>
