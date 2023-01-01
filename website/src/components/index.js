392ac82a5f6b3e754db1345b9b9909e2b49e72bd
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-01-01 17:29:20 -0600

    Export all components from a single file

---
+++ website/src/components/index.js
@@ -0,0 +1,8 @@
+// in this file I will export all the components that I will use in the app
+// so this way I dont write a line of code in the App.js for each component
+
+export { default as Article } from './article/Article';
+export { default as Brand } from './article/Brand';
+export { default as CTA } from './article/CTA';
+export { default as Feature } from './article/Feature';
+export { default as Navbar } from './article/Navbar';
