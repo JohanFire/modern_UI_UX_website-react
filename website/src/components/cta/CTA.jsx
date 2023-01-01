add5e00324914fb1c73afa80ffbc0d790bf4dbec
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-01-01 17:25:36 -0600

    Creating all necessary Components

---
+++ website/src/components/cta/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './CTA.css'
+
+const CTA = () => {
+    return (
+        <div>
+            CTA (Call To Action)
+        </div>
+    )
+}
+
+export default CTA
